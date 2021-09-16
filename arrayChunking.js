a2dbffcb4146af0b1512bcfab11c36e42ce7895d
Apurva Wadekar <apurva.w17@gmail.com>
2021-09-16 08:26:34 +0530

    array chunking

--- arrayChunking.js
+++ arrayChunking.js
@@ -7,3 +7,29 @@
 // chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
+
+function chunkedArray(arr, size) {
+  const chunked = [];
+  let index = 0;
+  while (index < arr.length) {
+    chunked.push(arr.slice(index, index + size));
+    index += size;
+  }
+
+  return chunked;
+}
+
+// function chunkedArray(arr, size) {
+//   const chunked = [];
+//   for (let element of arr) {
+//     const last = chunked[chunked.length - 1];
+//     if (!last || last.length === size) {
+//       chunked.push([element]);
+//     } else {
+//       last.push(element);
+//     }
+//   }
+//   return chunked;
+// }
+
+console.log(chunkedArray([1, 2, 3, 4, 5, 6], 2));
